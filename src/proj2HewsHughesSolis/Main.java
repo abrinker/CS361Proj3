634a74f1da8ff4c89a041d35eea912ee1307bfbd
Alex Rinker <abrinker@colby.edu>
2016-10-01 12:55:34 -0400

    refactored most of the code to be more elegant. e.g. more fxml

--- src/proj2HewsHughesSolis/Main.java
+++ src/proj2HewsHughesSolis/Main.java
@@ -15,16 +15,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
-import javafx.scene.control.TextInputDialog;
 import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseEvent;
-import javafx.event.EventHandler;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Optional;
 
 /**
  * Creates application to play major scale given a starting note.
@@ -40,7 +36,7 @@ public class Main extends Application {
      * Sets up the FXML elements that are dynamically built
      */
     public void initialize() {
-        setupCompositionSheet();
+        createCompositionSheet();
     }
 
     /**
@@ -64,22 +60,6 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.setOnCloseRequest(event -> System.exit(0));
         primaryStage.show();
-    }
-
-    /**
-     * Initializes the composition sheet by drawing it and setting up
-     * the proper mouseEvents that allow the user to interact with it
-     *
-     * @param root (the main StackPane for our scene)
-     */
-    private void setupCompositionSheet() {
-        createCompositionSheet();
-        this.compositionSheet.addEventFilter(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                addNoteToComposition(mouseEvent.getX(), mouseEvent.getY());
-            }
-        });
     }
 
     /**
@@ -107,34 +87,19 @@ public class Main extends Application {
      * @param yPos the input y position of the note (will be adjusted to look nice)
      */
     private void addNoteToComposition(double xPos, double yPos) {
-        Rectangle note = new Rectangle(25.0, 10.0, Color.BLUE);
-        note.setX(xPos); note.setY(yPos - (yPos % 10));
-        this.compositionSheet.getChildren().add(note);
+        if (yPos > 0 && yPos < 1270) {
+            Rectangle note = new Rectangle(25.0, 10.0, Color.BLUE);
+            note.setX(xPos); note.setY(yPos - (yPos % 10));
+            this.compositionSheet.getChildren().add(note);
+        }
     }
 
+    @FXML
     /**
-     * Play a major scale. First prompts the user to input a start note, after which the major scale starting at that
-     * note is played
-     *
-     * */
-    private void playScale(){
-        TextInputDialog dialog = new TextInputDialog("55");
-        dialog.setTitle("Starting Note");
-        dialog.setHeaderText("Please give me a starting note (0-115):");
-        Optional<String> result = dialog.showAndWait();
-        if (result.isPresent()) {
-            int startNote = Integer.valueOf(result.get());
-
-            int[] notes = constructMajorScale(startNote);
-
-            this.midiPlayer.stop();
-            this.midiPlayer.clear();
-            for (int i = 0; i< notes.length; i++) {
-                midiPlayer.addNote(notes[i], 100, i, 1, 0, 0);
-            }
-
-            this.midiPlayer.play();
-        }
+     * Adds a note to the composition panel
+     */
+    protected void handleAddNote(MouseEvent mouseEvent) {
+        addNoteToComposition(mouseEvent.getX(), mouseEvent.getY());
     }
 
     @FXML
@@ -169,28 +134,7 @@ this.     * For use in our stop button
      * @param event the event which should trigger the dialog box and midiplayer combo functionality.
      */
     protected void handlePlayMidi(ActionEvent event) {
-        playScale();
-    }
 
-    /**
-     * Construct a major scale from the starting pitch
-     *
-     * @param startNote the note to begin the scale from.
-     *
-     * @return The sequence of note defining the scale, going up and down.
-     * */
-    private int[] constructMajorScale(int startNote){
-        // Construct the sequence of notes defining the major scale.
-        int[] scaleUp = {startNote, startNote + 2, startNote + 4, startNote + 5,
-                         startNote + 7, startNote + 9, startNote + 11, startNote + 12};
-
-        // Create the sequence going up and down.
-        int[] scaleUpDown = new int[16];
-        for(int i=0; i<scaleUp.length; i++){
-            scaleUpDown[i] = scaleUp[i]; // placement in up sequence
-            scaleUpDown[scaleUpDown.length - (i+1)] = scaleUp[i]; // reverse in down sequence
-        }
-        return scaleUpDown;
     }
 
     /**
